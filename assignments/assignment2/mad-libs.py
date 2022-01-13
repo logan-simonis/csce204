b1cd001f7e19fdffb33511a3e746504720dc869d
logan-simonis <97473086+logan-simonis@users.noreply.github.com>
2022-01-13 09:46:18 -0500

    Add files via upload

---
+++ assignments/assignment2/mad-libs.py
@@ -0,0 +1,18 @@
+# Creating a madlib program
+# Author: Logan Simonis
+
+numOne = input("Number: ")
+numTwo = input("Another Number: ")
+liquid = input("Liquid: ")
+animal = input("Animal: ")
+numThree = input("Another Number: ")
+firstAdj = input("Adjective: ")
+sweetFood = input("Sweet Food: ")
+numFour = input("Another Number: ")
+secondAdj = input("Another Adjective: ")
+print("")
+print("**Mom's Famouse Receipe!**")
+print("")
+print(f"""First, preheat the oven to {numOne} degrees. Then beat {numTwo} {animal} egg(s) with a cup of {liquid}.
+ Next add {numThree} teaspoon(s) of baking soda and mix with eggs and {liquid} until it reaches a {firstAdj} texture.
+Don't forget the {sweetFood}! Pour into a 9 by 9 pan and bake for {numFour} minute(s) or until the cake is {secondAdj}.""")
