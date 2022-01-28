7d68c39478c5605ab2bc391d81498e65856b6dd0
logan-simonis <97473086+logan-simonis@users.noreply.github.com>
2022-01-28 15:14:05 -0500

    Add files via upload

---
+++ assignments/assignment6/zodiac_signs.py
@@ -0,0 +1,60 @@
+# Author: Logan Simonis
+# Fortune Telling
+
+print("***** Monthly Fortune Telling *****")
+options = input("What would you like to do? (V)iew Signs, or see class (F)ortune: ")
+print()
+if options == "v":
+    print("Astrological Signs and Dates:")
+    print("- Aries: March 21 - April 19")
+    print("- Taurus: April 20 - May 20")
+    print("- Gemini: May 21 - June 21")
+    print("- Cancer: June 22 - July 22")
+    print("- Leo: July 23 - August 22")
+    print("- Virgo: August 23 - September 22")
+    print("- Libra: September 23 - October 23")
+    print("- Scorpius: October 24 - November 21")
+    print("- Sagittarius: November 22 - December 21")
+    print("- Capricoronus: December 22 - January 19")
+    print("- Aquarius: January 20 - February 18")
+    print("- Pisces: February 19 - March 20")
+if options == "f":
+    sign = input("Enter your sign: ")
+if sign == "Aries":
+        print("You will make a lot of money today!")
+        print("Have a nice day")
+if sign == "Taurus":
+    print("You will make a new friend today.")
+    print("Have a nice day")
+if sign == "Gemini":
+    print("You have to go hit the gym today.")
+    print("Have a nice day")
+if sign == "Cancer":
+    print("follow your heart, it will lead you to where you need to go.")
+    print("Have a nice day")
+if sign == "Leo":
+    print("You have a heart of gold!")
+    print("Have a nice day")
+if sign == "Virgo":
+    print("You will have the best year of your life.")
+    print("Have a nice day")
+if sign == "Libra" :
+    print("Do what makes you happy!")
+    print("Have a nice day")
+if sign == "Scorpius":
+    print("Realize what your goals are and chase after them!")
+    print("Have a nice day")
+if sign == "Sagittarius":
+    print("Try something you've never done before today.")
+    print("Have a nice day")
+if sign == "Capricoronus":
+    print("Never give up!")
+    print("Have a nice day")
+if sign == "Aquarius":
+    print("You will get a much needed vacation this week!")
+    print("Have a nice day")
+if sign == "Pisces":
+    print("Someone will give you some wise advice this week.")
+    print("Have a nice day")
+
+    
